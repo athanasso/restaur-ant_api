91db122fcc665157b10f480284125af5925daf0a
Emmanouil Athanasopoulos <70025614+athanasso@users.noreply.github.com>
2024-08-21 15:56:08 +0300

    Update reviews.module.ts

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -3,10 +3,19 @@ import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { Review } from 'src/entities/review.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolesGuard } from 'src/guards/role.guard';
+import { Reflector } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review])],
-  providers: [ReviewsService],
-  controllers: [ReviewsController]
+  imports: [
+    TypeOrmModule.forFeature([Review]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || '',
+      signOptions: { expiresIn: '20m' },
+    }),
+  ],
+  providers: [ReviewsService, RolesGuard, Reflector],
+  controllers: [ReviewsController],
 })
 export class ReviewsModule {}
