dc99739c8fcb99dd875b94469264f3c49eed1a92
Emmanouil Athanasopoulos <70025614+athanasso@users.noreply.github.com>
2024-09-06 00:52:48 +0300

    AllowedHeader added

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
 
